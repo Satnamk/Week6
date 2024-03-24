3589e8a785e8bc9bbadf7ac31f7d2a96cb76aba0
Satnam Kaur <Satnam.NA@metropolia.fi>
2024-03-24 12:12:37 +0200

    Postman

--- extra/index.js
+++
@@ -1,8 +0,0 @@
-const user = { id: 42,
-isVerified: true,
-firstname: "Satnam",
-lastname: "Kaur"
-};
-
-const { firstname, lastname, id,  isVerified} = user;
-console.log( "firstname:" , firstname, lastname, id, isVerified );
